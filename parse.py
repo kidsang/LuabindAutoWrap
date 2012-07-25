07e7619637da1cdb77e2381fbbc84536351058d9
kid <963262214@qq.com>
2012-07-25 16:29:37 +0800

    v0.3 增加了判断public区域

--- parse.py
+++ parse.py
@@ -6,12 +6,35 @@ from construct import *
 ignore = ['inline']
 # 支持重载的运算符
 op = ['+', '-', '*', '/', '==', '<', '<=']
+# public区域
+pubArea = []
 
 # 辅助方法，返回函数参数列表
 # string: 含有参数列表的字符串
 def getParams(string):
     params = re.search(r'\((.*)\)', string).group(1)
     return params.split(',')
+
+# 寻找public区域
+# node: 类根节点
+# lines: 文件原始内容
+def findPublicArea(node, lines):
+    global pubArea
+    pubArea = []
+    pub = False
+    b = -1
+    for c in node.get_children():
+        if str(c.kind) == 'CursorKind.CXX_ACCESS_SPEC_DECL':
+            if lines[c.location.line - 1].find('public') != -1:
+                if pub == False:
+                    b = c.location.line
+                pub = True
+            elif pub == True:
+                pubArea.append([b, c.location.line])
+                b = -1
+                pub = False
+    if b != -1:
+        pubArea.append([b, 999999])    
 
 # 寻找类的构造函数
 # className: 类名
@@ -20,6 +43,14 @@ def findConstructors(className, node):
     global indent
     for c in node.get_children():
         if str(c.kind) == 'CursorKind.CONSTRUCTOR':
+            # 判断public区域
+            pub = False
+            for a in pubArea:
+                if a[0] < c.location.line < a[1]:
+                    pub = True
+                    break
+            if pub == False:
+                continue
             # 分离参数列表
             params = getParams(c.displayname)
             print defConstructor(params)
@@ -33,8 +64,15 @@ def findMethod(className, node, lines):
     global indent
     for c in node.get_children():
         if str(c.kind) == 'CursorKind.CXX_METHOD':
-            if str(c.spelling).find('operator') == -1: # 非运算符重载
+            # 判断public区域
+            pub = False
+            for a in pubArea:
+                if a[0] < c.location.line < a[1]:
+                    pub = True
+                    break
+            if pub == False:
                 continue
+            if str(c.spelling).find('operator') == -1: # 非运算符重载
                 # 查找返回值
                 ret = lines[c.location.line-1] \
                       [:c.location.column - 1]
@@ -69,6 +107,7 @@ def findClass(node, name, lines):
             and str(node.spelling) == name \
             and node.is_definition():
         print defClass(name)
+        findPublicArea(node, lines)
         findConstructors(name, node)
         findMethod(name, node, lines)
 
